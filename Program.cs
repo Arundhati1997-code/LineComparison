ea88c37c602fc062bbdb94842ce30ed5add05243
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-10-07 13:59:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LineComparison;
+
+Console.WriteLine("Hello, World!");
+//uc1.Length1(20,30,12,14);
+//Console.ReadKey();
+//uc3.Length();
+uc2.Length2();
