0499bec9e571e78d7b49050b638de6e2db99ba3d
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-10-07 14:00:42 +0530

    Add files via upload

---
+++ uc1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class uc1
+    {
+        public static int Length1(int X1, int Y1, int X2, int Y2)
+        {
+            Console.WriteLine("welcome to line comparision");
+             
+            int length = ((int)Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2)));
+
+            Console.WriteLine("\n\n Length of line is" + length);
+            Console.ReadLine();
+            return length;
+        }
+
+        
+    }
+}
+
